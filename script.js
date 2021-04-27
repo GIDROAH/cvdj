249652392487c47a940dad101561a77a657d72d6
Deepak Jaiswar <DeepakJaiswar678@gmail.com>
2021-04-27 16:31:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 /*
-lertt("Hello \n I am Deepak Jaiswar \n Thanks for visiting here. \n Press ok to view profile.");
+alert("Hello \n I am Deepak Jaiswar \n Thanks for visiting here. \n Press ok to view profile.");
+*/
