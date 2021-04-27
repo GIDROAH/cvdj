4b1d33062b96c6aa79d3c2427c5f1addeb8ff904
Deepak Jaiswar <DeepakJaiswar678@gmail.com>
2021-04-27 13:25:17 +0530

    Add files via upload
    added python

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-alert("Hello \n I am Deepak Jaiswar \n Thanks for visiting here. \n Press ok to view profile.");
+/*
+lertt("Hello \n I am Deepak Jaiswar \n Thanks for visiting here. \n Press ok to view profile.");
